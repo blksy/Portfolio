5b44f1f608b88d0b266d45eeccc6ecdba0b0d7f4
blksy <79380937+blksy@users.noreply.github.com>
2022-09-10 21:45:43 +0200

    added particles to background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
+import Particles from './Particles';
 import Navigation from './screens/navigationBar/NavigationBar';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Particles/>
     </div>
   );
 }
