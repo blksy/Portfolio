59f1cc1c585d2a08ce4f1c7269b4297092f74121
blksy <79380937+blksy@users.noreply.github.com>
2022-09-30 17:18:00 +0200

    added images for portfolio and created portfolioData.js responsible for keeping an object with projects details

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,53 @@
+import clinic from '../images/projectsicon/clinic.gif'
+import iceCream from '../images/projectsicon/iceCream.gif'
+import movies from '../images/projectsicon/movies.gif'
+import webStudio from '../images/projectsicon/webStudio.gif'
+import tours from '../images/projectsicon/touringnat.gif'
+import weather from '../images/projectsicon/weather.gif'
+
+const portfolioData = [
+    {
+      image:clinic,
+      link:'https://lovely-sopapillas-735c3d.netlify.app/',
+      desc:'Clinic webiste',
+      summary:'Clinic website made for practice purposes ',
+      tech:'HTML, CSS',
+    },
+    {
+        image:iceCream,
+        link:'https://blksy.github.io/IceCream/',
+        desc:'Ice Cream',
+        summary:'Ice Cream company website',
+        tech:'HTML, CSS, SCSS',
+      },
+      {
+        image:movies,
+        link:'https://blksy.github.io/goit-react-hw-05-movies/',
+        desc:'Movies',
+        summary:'Application for searching movies with details',
+        tech:'JavaScript, HTML, CSS',
+      },
+      {
+        image:webStudio,
+        link:'https://blksy.github.io/Website/',
+        desc:'Web Studio',
+        summary:'Website for company offering business solutions',
+        tech:'HTML, CSS, JavaScript',
+      },
+      {
+        image:tours,
+        link:'https://blksy.github.io/Practice_project_natours/',
+        desc:'Tour ',
+        summary:'Website for tour agency',
+        tech:'HTML, CSS, SASS',
+      },
+      {
+        image:weather,
+        link:'https://blksy.github.io/Weather_app/',
+        desc:'',
+        summary:'Simple weather application that uses accuweather',
+        tech:'HTML, CSS, JavaScript',
+      },
+]
+
+export default portfolioData;   
